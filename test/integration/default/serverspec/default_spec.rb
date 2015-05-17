7bf32d846c02454ceb55d5ed01fa97919c72e289
Dimos Karagiannis <mr.dimka@gmail.com>
2015-05-17 12:08:10 +0300

    adding apt recipe on ubuntu 12.04 platform, minor changes on test

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -5,13 +5,14 @@ describe 'MyApp_mysqldb::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
   
+ it 'mysql service should be enabled and running' do
+ 	expect(service(mysql)).to be_enabled
+ 	expect(service(mysql)).to be_running
+ end
+
  it 'mysql is listening on port 3306' do
     expect(port(3306)).to be_listening
   end
 
- describe service('mysql') do
- 	it { should be_enabled }
- 	it { should be_running }
- end
 
 end
